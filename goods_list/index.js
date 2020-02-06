80f58d6066fe4f25a508bf5afe778d7928a08ad9
778464081 <56761279+778464081@users.noreply.github.com>
2020-02-06 19:57:50 +0800

    小洪优购商品列表----未完待续

---
+++ goods_list/index.js
@@ -0,0 +1,46 @@
+// pages/goods_list/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabs:[
+      {
+        id:0,
+        value:"综合",
+        isActive:true
+      },
+      {
+        id:1,
+        value:"销量",
+        isActive:false
+      },
+      {
+        id:3,
+        value:"价格",
+        isActive:false
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+      console.log(options);
+  },
+    //标题点击事件 从子组件传递过来
+    handleTabsItemChange(e){
+      //1 获取被点击的标题索引
+      const {index}=e.detail;
+      //2 修改源数组
+      let {tabs}=this.data;
+      tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
+      //3 赋值到data中
+      this.setData({
+        tabs
+      })
+    }
+  
+})
