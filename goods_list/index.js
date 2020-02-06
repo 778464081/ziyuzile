28c46e0b563430c445cc8d339196a0ac01d15105
778464081 <56761279+778464081@users.noreply.github.com>
2020-02-06 20:42:20 +0800

    小洪优购商品列表--大致完成

--- goods_list/index.js
+++ goods_list/index.js
@@ -1,4 +1,4 @@
-// pages/goods_list/index.js
+import { request } from "../../request/index.js";
 Page({
 
   /**
@@ -21,15 +21,32 @@ Page({
         value:"价格",
         isActive:false
       }
-    ]
+    ],
+    goodsList:[],
+  },
+  //接口要的参数
+  QuerryParams:{
+    query:"",
+    cid:"",
+    pagenum:1,
+    pagesize:10
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      console.log(options);
+     this.QuerryParams.cid=options.cid;
+     this.getGoodsList();
   },
+  //获取商品列表数据
+  async getGoodsList(){
+    const res=await request({url:"/goods/search",data:this.QuerryParams});
+    this.setData({
+      goodsList:res.goods,
+    })
+  },
+
     //标题点击事件 从子组件传递过来
     handleTabsItemChange(e){
       //1 获取被点击的标题索引
