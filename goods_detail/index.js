a01524200ab1c9e405331d867cf3c3c11af0de29
778464081 <56761279+778464081@users.noreply.github.com>
2020-02-08 21:59:01 +0800

    小洪优购商品详情页

--- goods_detail/index.js
+++ goods_detail/index.js
@@ -1,3 +1,17 @@
+/*
+  预览swiper 图片
+  1 发送请求获取数据
+  2 点击轮播图 预览大图
+    1 给轮播图绑定点击事件
+    2 调用小程序的 api previewImage
+  3 点击 加入购物车
+    1 先绑定点击事件
+    2 获取缓存缓存中的购物车数据 数组格式
+    3 先判断 当前的商品是否已经存在于 购物车
+    4 已经存在 修改商品数据 执行购物车数量++ 重新把购物车数组 填充回缓存中
+    5 不存在购物车数组中 直接给购物车数组添加一个新元素 新元素 带上一个购买数量属性 num 重新把购物车数组填充回缓存中
+    6 弹出提示
+    */
 import { request } from "../../request/index.js";
 Page({
 
@@ -8,6 +22,8 @@ Page({
     goodsObj:{}
 
   },
+  //商品对象
+  GoodsInfo:[],
 
   /**
    * 生命周期函数--监听页面加载
@@ -20,8 +36,53 @@ Page({
   //获取商品详细数据
   async getGoodsDetail(goods_id){
     const goodsObj=await request({url:"/goods/detail",data:{goods_id}});
+    this.GoodsInfo=goodsObj;
     this.setData({
-      goodsObj
+      goodsObj:{
+       
+        goods_name:goodsObj.goods_name,
+        goods_price:goodsObj.goods_price,
+        //iphone 部分手机 不识别 webp图片样式
+        goods_introduce:goodsObj.goods_introduce.replace(/\.webp/g,'.jpg'),
+        pics:goodsObj.pics
+      }
     })
+  },
+  //点击轮播图 放大预览
+  handlePreviewImage(e){
+    // 1 先构造要预览的图片数组
+    const urls=this.GoodsInfo.pics.map(v=>v.pics_mid);
+    // 2 接受传递过来的图片url
+    const current=e.currentTarget.dataset.url;
+    wx.previewImage({
+      current,
+      urls
+    })
+  },
+  //点击 加入购物车
+  handleCartAdd(){
+    //1 获取缓存中的购物车数组
+    let cart=wx.getStorageSync("cart")||[];
+    //2 判断 商品对象是否存在于购物车数组中
+    let index=cart.findIndex(v=>v.goods_id===this.GoodsInfo.goods_id);
+    if(index===-1){
+      // 3 不存在
+      // console.log("不存在");
+      this.GoodsInfo.num=1;
+      cart.push(this.GoodsInfo);
+    }else{
+      //4 已经存在购物车数据 执行num++
+      // console.log("存在")
+      cart[index].num++;
+    }
+    //5 把购物车重新添加回缓存中
+    wx.setStorageSync("cart",cart);
+    //6 弹窗提示
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+    //true 防止用户手抖 疯狂点击按钮
+      mask: true,
+    });
   }
 })
