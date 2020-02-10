589212327e70b46d2c60f27dfa09397591f726d8
778464081 <56761279+778464081@users.noreply.github.com>
2020-02-10 23:10:40 +0800

    购物车--完善

--- carts/index.js
+++ carts/index.js
@@ -16,19 +16,43 @@
 2 页面加载完毕
   1 获取本地存储中的地址数据
   2 把数据 设置给data中的一个变量
+3 onShow
+  1 获取缓存中的购物车数组
+  2 把购物车数据 填充到data中
+4 全选的实现 数据的展示
+  1 onShow 获取缓存中的购物车数组
+  2 根据购物车中的商品数据 所以的商品都被选中 checked=true 全选就被选中
+5 总价格和总数量
+  1 都需要商品被选中 
+  2 获取购物车数组
+  3 判断商品是否被选中
+  4 总价格+=商品的单价*商品的数量
+  5 总数量+=商品的数量
+  6 把计算后的的价格和数量 设置回data中即可
+6 商品的选中
+  1 绑定change事件
+  2 获取到被修改的商品对象
+  3 商品对象的选中状态 取反
+  4 重新填充回data中和缓存中
+  5 重新计算全选，总价格，总数量
+
 */ 
 import { getSetting, openSetting, chooseAddress} from "../../utils/asyncWx.js";
 Page({
   data:{
       address:{},
+      cart:[],
+      allChecked:false,
+      totalPrice:0,
+      totalNum:0,
   },
   onShow(){
     //1 获取缓存中的收货地址信息
     const address = wx.getStorageSync('address');
-    //2 给data赋值
-    this.setData({
-      address
-    })
+    //1 获取缓存中的购物车数据
+    const cart=wx.getStorageSync('cart') || [];
+    this.setData({address});
+    this.setCart(cart);
   },
   //点击 收货地址
  async handleChooseAdress(){
@@ -82,5 +106,39 @@ Page({
     console.log(error);
     
   }
-}
+  },
+ 
+  handleItemchange(e){
+    //1 获取被修改的商品的ID
+    const goods_id=e.currentTarget.dataset.id;
+    //2 获取购物车数组
+    let {cart} =this.data;
+    //3 找到被修改的商品对象
+    let index=cart.findIndex(v=>v.goods_id===goods_id);
+    //4 选中状态取反
+    cart[index].checked=!cart[index].checked;
+    this.setCart(cart);
+  },
+  // 设置购物车状态 重新计算 底部工具栏的数据 全选 总价格 购买的数据
+  setCart(cart){
+    let allChecked=true
+    //1 总价格 总数量
+    let totalPrice=0;
+    let totalNum=0;
+    cart.forEach(v=> {
+      if(v.checked){
+      totalPrice+=v.num*v.goods_price;
+      totalNum+=v.num;
+      }else{
+        allChecked=false;
+      }
+    });
+    //判断数组是否为空
+    allChecked=cart.length != 0 ? allChecked : false;
+    this.setData({
+      cart,
+      totalPrice,totalNum,allChecked,
+    });
+    wx.setStorageSync("cart", cart);
+  }
 })
