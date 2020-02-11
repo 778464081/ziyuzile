d5171303a0916d4a27e651af18be96522ee9958b
778464081 <56761279+778464081@users.noreply.github.com>
2020-02-11 16:13:31 +0800

    小洪优购购物车页

--- carts/index.js
+++ carts/index.js
@@ -35,9 +35,28 @@
   3 商品对象的选中状态 取反
   4 重新填充回data中和缓存中
   5 重新计算全选，总价格，总数量
-
+7 全选和反选
+  1 全选复选框绑定事件change
+  2 获取 data 中的全选变量 allChecked
+  3 直接取反 allChecked=!allChecked
+  4 遍历购物车数组 让里面 商品选中状态跟随 allChecked 改变而改变
+  5 把购物车数组 和allChecked重新设置回data 把购物车重新设置回缓存中
+8 商品数量的编辑
+  1 + - 按钮 绑定同一个点击事件 区分的关键 自定义属性
+  2 传递被点击的商品id goods_id
+  3 获取data中的购物车数组 来获取需要被修改的商品对象
+    当购物车的数量=1 同时用户点击"-",
+    弹窗提示（wx.showModal） 询问用户 是否要删除
+      1 确定 直接执行删除
+      2 取消 什么都不做
+  4 直接修改商品对象的数量num
+  5 把cart数组 重新设置回 缓存中 和data中 this.setCart
+9 点击结算
+  1 判断有没有收获地址信息
+  2 判断用户有没有选择商品
+  3 经过以上的验证 跳转到支付界面
 */ 
-import { getSetting, openSetting, chooseAddress} from "../../utils/asyncWx.js";
+import { getSetting, openSetting, chooseAddress,showModal,showToast} from "../../utils/asyncWx.js";
 Page({
   data:{
       address:{},
@@ -140,5 +159,57 @@ Page({
       totalPrice,totalNum,allChecked,
     });
     wx.setStorageSync("cart", cart);
-  }
+  },
+  //商品全选功能
+  handleItemAllchange(){
+    // 1 获取data中的数组
+    let {cart,allChecked}=this.data;
+    //2 修改值
+    allChecked=!allChecked;
+    //3 循环修改cart数组 中的商品选中状态
+    cart.forEach(v=>v.checked=allChecked);
+    //4 把修改后的值 填充回data或者缓存中
+    this.setCart(cart);
+  },
+  //商品数量的编译功能
+ async handleItemNumEdit(e){
+    // 1 获取传递过来的参数
+    const {operation,id}=e.currentTarget.dataset;
+    //2 获取购物车数组
+    let {cart}=this.data;
+    //3 找到需要修改的商品的索引
+    const index=cart.findIndex(v=>v.goods_id===id);
+      //判断是否要执行删除
+      if(cart[index].num===1&&operation===-1){
+        const result=await showModal({content:"您是否要删除？"});
+        if(result.confirm){
+          cart.splice(index,1);
+          this.setCart(cart);
+         }
+      }else{
+    //4 进行修改数量
+    cart[index].num+=operation;
+    //5 设置回缓存和data中
+    this.setCart(cart);
+         }
+  },
+  // 商品数量的编辑功能
+ async handlePay(){
+    //1 判断收获地址
+    const {address,totalNum}=this.data;
+    if(!address.userName){
+      await showToast({title:"您还没有选择收货地址"});
+      console.log(address.userName);
+      return;
+    }
+    //2 判断用户是否选择商品
+    if(totalNum===0){
+      await showToast({title:"您还没有选择商品"});
+      return;
+    }
+    //3 跳转到 支付页面
+    wx.navigateTo({
+      url:'/pages/pay/index'
+    })
+  },
 })
